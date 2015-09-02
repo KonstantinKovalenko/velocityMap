6e607d9172670a325c3c493e9b50bf6f7c90f47e
Konstantin Kovalenko <red_nada@mail.ru>
2015-09-02 17:45:16 +0300

    2.0 r 100 000

--- src/velocity/Velocity.java
+++ src/velocity/Velocity.java
@@ -1,117 +1,103 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package velocity;
 
 import java.util.*;
 import java.util.concurrent.*;
 import static mytoys.Print.*;
 
-class MyMap{
-    private Random rand = new Random();
+class MyMap {
+
     public final int MAP_SIZE;
-    public MyMap(int mapSize){
+
+    public MyMap(int mapSize) {
         MAP_SIZE = mapSize;
     }
-    public void fillMap(Map<Integer,Integer> map){
+
+    public void fillMap(Map<Integer, Integer> map) {
         long timer = -System.currentTimeMillis();
-        for(int i = 0;i<MAP_SIZE;i++ )
-            map.put(i, rand.nextInt(MAP_SIZE));
+        for (int i = 0; i < MAP_SIZE; i++) {
+            map.put(i, (int) (Math.random() * MAP_SIZE));
+        }
         timer += System.currentTimeMillis();
-        println(map.getClass().getSimpleName() + ".fillMap() in "+timer+" ms");
+        println(map.getClass().getSimpleName() + ".fillMap() in " + timer + " ms");
     }
-    public void clearHalfMap(int zeroOneSwitcher, Map<Integer,Integer> map){
-        switch(zeroOneSwitcher){
-            case 0:{
-                long timer = -System.currentTimeMillis();
-                for(int i = 0;i<MAP_SIZE;i++){
-                    if(i%2==0)
-                        map.remove(i);
+
+    public void clearRandomQuantity(int quantity, Map<Integer, Integer> map) {
+        int workQuantity = quantity;
+        long timer = -System.currentTimeMillis();
+        while (workQuantity-- != 0) {
+            boolean breaker = true;
+            Integer mapRemove = map.remove((int) (Math.random() * MAP_SIZE));
+            if (mapRemove == null) {
+                while (breaker) {
+                    mapRemove = map.remove((int) (Math.random() * MAP_SIZE));
+                    if (mapRemove != null) {
+                        breaker = false;
+                    }
                 }
-                timer += System.currentTimeMillis();
-                println(map.getClass().getSimpleName() + ".clearHalfMap(even int) in "+timer+" ms");
-                return;
-            }
-            case 1:{
-                long timer = -System.currentTimeMillis();
-                for(int i = 0;i<MAP_SIZE;i++){
-                    if(i%2!=0)
-                        map.remove(i);
-                }
-                timer += System.currentTimeMillis();
-                println(map.getClass().getSimpleName() + ".clearHalfMap(odd int) in "+timer+" ms");
-                return;
             }
         }
-    }
-    public void clearMap(Map<Integer,Integer> map){
-        long timer = -System.currentTimeMillis();
-        map.clear();
         timer += System.currentTimeMillis();
-        println(map.getClass().getSimpleName() + ".clear() in "+timer+" ms");
+        println(map.getClass().getSimpleName() + ".clearRandomQuantity(" + quantity + ") in " + timer + " ms");
     }
-    public void getElementByKey(int element,Map<Integer,Integer> map){
-        long timer = -System.currentTimeMillis();
-        map.get(element);
-        timer += System.currentTimeMillis();
-        println(map.getClass().getSimpleName() + ".getElementByKey("+element+") in "+timer+" ms");
-    }
-    public void getAllElements(Map<Integer,Integer> map){
-        try{
+
+    public void getRandomElements(int numberOfElements, Map<Integer, Integer> map) {
+        try {
             long timer = -System.currentTimeMillis();
+            int workNumberOfElements = numberOfElements;
             String itsALongString = "";
-            for(int i : map.keySet())
-                itsALongString += map.get(i).toString();
+            while (workNumberOfElements-- != 0) {
+                itsALongString += map.get((int) (Math.random() * MAP_SIZE)).toString();
+            }
             timer += System.currentTimeMillis();
-            println(map.getClass().getSimpleName() + ".getAllElements() in "+timer+" ms");
-        }catch(NullPointerException e){
-            System.err.println(Thread.currentThread().getName()+".getAllElements() failed");
+            println(map.getClass().getSimpleName() + ".getRandomElements(" + numberOfElements + ") in " + timer + " ms");
+        } catch (NullPointerException e) {
+            System.err.println(Thread.currentThread().getName() + ".getRandomElements(" + numberOfElements + ") failed");
         }
     }
 }
+
 public class Velocity {
-    private static Map<Integer,Integer> treeMap= new TreeMap<>();
-    private static Map<Integer,Integer> enumMap= new ConcurrentSkipListMap<>();
-    private static Map<Integer,Integer> hashMap= new HashMap<>();
-    private static Map<Integer,Integer> linkedHashMap= new LinkedHashMap<>();
-    private static Map<Integer,Integer> weakHashMap= new WeakHashMap<>();
-    private static Map<Integer,Integer> concurrentHashMap= new ConcurrentHashMap<>();
-    private static Map<Integer,Integer> identityHashMap= new IdentityHashMap<>();
-    private static List<Map<Integer,Integer>> mapList = new LinkedList<>();
+
+    private static Map<Integer, Integer> treeMap = new TreeMap<>();
+    private static Map<Integer, Integer> concurrentSkipListMap = new ConcurrentSkipListMap<>();
+    private static Map<Integer, Integer> hashMap = new HashMap<>();
+    private static Map<Integer, Integer> linkedHashMap = new LinkedHashMap<>();
+    private static Map<Integer, Integer> weakHashMap = new WeakHashMap<>();
+    private static Map<Integer, Integer> concurrentHashMap = new ConcurrentHashMap<>();
+    private static Map<Integer, Integer> identityHashMap = new IdentityHashMap<>();
+    private static List<Map<Integer, Integer>> mapList = new LinkedList<>();
     private static Thread t;
-    private static void fillMapList(){
-        mapList.add(treeMap);
-        mapList.add(enumMap);
-        mapList.add(hashMap);
-        mapList.add(linkedHashMap);
-        mapList.add(weakHashMap);
-        mapList.add(concurrentHashMap);
-        mapList.add(identityHashMap);
-        
+
+    private static void fillMapList(List<Map<Integer, Integer>> workList) {
+        workList.add(treeMap);
+        workList.add(concurrentSkipListMap);
+        workList.add(hashMap);
+        workList.add(linkedHashMap);
+        //workList.add(weakHashMap);
+        workList.add(concurrentHashMap);
+        //workList.add(identityHashMap);
     }
-    
+
     public static void main(String[] args) {
-        MyMap myMap = new MyMap(10000);
-        fillMapList();
-        for(Map<Integer,Integer> map : mapList){
+        MyMap myMap = new MyMap(100000);
+        fillMapList(mapList);
+        for (Map<Integer, Integer> map : mapList) {
             myMap.fillMap(map);
-            myMap.getElementByKey(new Random().nextInt(myMap.MAP_SIZE),map);
             println();
-            t = new Thread(map.getClass().getSimpleName()){
-                public void run(){
-                    try{
-                        myMap.getAllElements(map);
-                        myMap.clearHalfMap(new Random().nextInt(2),map);
-                        myMap.clearMap(map);
-                        println();
+            t = new Thread(map.getClass().getSimpleName()) {
+                @Override
+                public void run() {
+                    myMap.getRandomElements((myMap.MAP_SIZE / 2), map);
+                    myMap.clearRandomQuantity((myMap.MAP_SIZE / 2), map);
+                    println();
+                    try {
                         TimeUnit.SECONDS.sleep(1);
-                    }catch(InterruptedException e){
-                        System.err.println(e+", sleep() interrupted");
+                    } catch (InterruptedException ignored) {
+
                     }
                 }
-            };t.start();
+            };
+            t.start();
         }
     }
 }
